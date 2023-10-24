e55488f92504e70e7b184b169a95cd5eeaca7d2f
Jorge <aportuvas@outlook.es>
2023-10-24 22:46:14 +0200

    add calendar-tw-ui

--- src/components/Calendario.jsx
+++ src/components/Calendario.jsx
@@ -1,35 +1,96 @@
-import './Calendario.css'
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import interactionPlugin from '@fullcalendar/interaction'
-import esLocale from '@fullcalendar/core/locales/es'
+import dayjs from 'dayjs'
+import React, { useState } from 'react'
+import { generateDate, months } from './util/calendar'
+import cn from './util/cn'
+import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
 
-const calendarEvents = [
-  {
-    title: 'Acto Inaugural y Presentación Curso 1',
-    start: '2023-10-21',
-    end: '2023-10-21',
-    color: 'hsl(120,50%,90%)',
-    textColor: 'hsl(120,50%,30%)'
-  },
-  {
-    title: 'Evento 2',
-    start: '2023-10-22',
-    end: '2023-10-28',
-    color: 'hsl(220,50%,90%)',
-    textColor: 'hsl(220,50%,30%)'
-  }
+const events = [
+  { dia: '26/10/2023', evento: 'Evento de Prueba' }
 ]
 
 export default function Calendar() {
+  const days = ['D', 'L', 'M', 'M', 'J', 'V', 'S']
+  const currentDate = dayjs()
+  const [today, setToday] = useState(currentDate)
+  const [selectDate, setSelectDate] = useState(currentDate)
   return (
-    <div className='calendar'>
-      <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin]}
-        initialView='dayGridMonth'
-        locale={esLocale}
-        events={calendarEvents}
-      />
+    <div className='flex gap-10 sm:divide-x justify-center sm:w-1/2 mx-auto  h-screen items-center sm:flex-row flex-col'>
+      <div className='w-96 h-96 '>
+        <div className='flex justify-between items-center'>
+          <h1 className='select-none font-semibold'>
+            {months[today.month()]}, {today.year()}
+          </h1>
+          <div className='flex gap-10 items-center '>
+            <GrFormPrevious
+              className='w-5 h-5 cursor-pointer hover:scale-105 transition-all'
+              onClick={() => {
+                setToday(today.month(today.month() - 1))
+              }}
+            />
+            <h1
+              className=' cursor-pointer hover:scale-105 transition-all'
+              onClick={() => {
+                setToday(currentDate)
+              }}>
+              Mes
+            </h1>
+            <GrFormNext
+              className='w-5 h-5 cursor-pointer hover:scale-105 transition-all'
+              onClick={() => {
+                setToday(today.month(today.month() + 1))
+              }}
+            />
+          </div>
+        </div>
+        <div className='grid grid-cols-7 '>
+          {days.map((day, index) => {
+            return (
+              <h1
+                key={index}
+                className='text-sm text-center h-14 w-14 grid place-content-center text-gray-500 select-none'>
+                {day}
+              </h1>
+            )
+          })}
+        </div>
+
+        <div className='grid grid-cols-7'>
+          {generateDate(today.month(), today.year()).map(
+            ({ date, currentMonth, today }, index) => {
+              return (
+                <div
+                  key={index}
+                  className='p-2 text-center h-14 grid place-content-center text-sm border-t'>
+                  <h1
+                    className={cn(
+                      currentMonth ? '' : 'text-gray-400',
+                      today ? 'bg-red-600 text-white' : '',
+                      selectDate.toDate().toDateString() ===
+                        date.toDate().toDateString()
+                        ? 'bg-black text-white'
+                        : '',
+                      'h-10 w-10 rounded-full grid place-content-center hover:bg-black hover:text-white transition-all cursor-pointer select-none'
+                    )}
+                    onClick={() => {
+                      setSelectDate(date)
+                    }}>
+                    {date.date()}
+                  </h1>
+                </div>
+              )
+            }
+          )}
+        </div>
+      </div>
+      <div className='h-96 w-96 sm:px-5'>
+        <h1 className=' font-semibold'>
+          Eventos para el{' '}
+          {selectDate.toDate().toLocaleDateString()}
+        </h1>
+        <p className='text-gray-400'>
+         Evento del día
+        </p>
+      </div>
     </div>
   )
 }
