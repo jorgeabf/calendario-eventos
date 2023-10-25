2e9f0d4f646a666f4e58ebee7613257b9c7fcd94
Jorge <aportuvas@outlook.es>
2023-10-25 23:50:01 +0200

    add: calendar tw-ui OK

--- src/components/Calendario.jsx
+++ src/components/Calendario.jsx
@@ -1,96 +1,299 @@
-import dayjs from 'dayjs'
-import React, { useState } from 'react'
-import { generateDate, months } from './util/calendar'
-import cn from './util/cn'
-import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
+import { Menu, Transition } from '@headlessui/react'
+import { DotsVerticalIcon } from '@heroicons/react/outline'
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon
+} from '@heroicons/react/solid'
+import {
+  add,
+  eachDayOfInterval,
+  endOfMonth,
+  format,
+  getDay,
+  isEqual,
+  isSameDay,
+  isSameMonth,
+  isToday,
+  parse,
+  parseISO,
+  startOfToday
+} from 'date-fns'
 
-const events = [
-  { dia: '26/10/2023', evento: 'Evento de Prueba' }
-]
+import { es } from 'date-fns/locale'
 
-export default function Calendar() {
-  const days = ['D', 'L', 'M', 'M', 'J', 'V', 'S']
-  const currentDate = dayjs()
-  const [today, setToday] = useState(currentDate)
-  const [selectDate, setSelectDate] = useState(currentDate)
+import { Fragment, useState } from 'react'
+
+import meetings from '../data/calendar.js'
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Calendario() {
+  let today = startOfToday()
+  let [selectedDay, setSelectedDay] = useState(today)
+  let [currentMonth, setCurrentMonth] = useState(
+    format(today, 'MMM-yyyy')
+  )
+  let firstDayCurrentMonth = parse(
+    currentMonth,
+    'MMM-yyyy',
+    new Date()
+  )
+
+  let days = eachDayOfInterval({
+    start: firstDayCurrentMonth,
+    end: endOfMonth(firstDayCurrentMonth)
+  })
+
+  function previousMonth() {
+    let firstDayNextMonth = add(firstDayCurrentMonth, {
+      months: -1
+    })
+    setCurrentMonth(format(firstDayNextMonth, 'MMM-yyyy'))
+  }
+
+  function nextMonth() {
+    let firstDayNextMonth = add(firstDayCurrentMonth, {
+      months: 1
+    })
+    setCurrentMonth(format(firstDayNextMonth, 'MMM-yyyy'))
+  }
+
+  let selectedDayMeetings = meetings.filter((meeting) =>
+    isSameDay(parseISO(meeting.startDatetime), selectedDay)
+  )
+
   return (
-    <div className='flex gap-10 sm:divide-x justify-center sm:w-1/2 mx-auto  h-screen items-center sm:flex-row flex-col'>
-      <div className='w-96 h-96 '>
-        <div className='flex justify-between items-center'>
-          <h1 className='select-none font-semibold'>
-            {months[today.month()]}, {today.year()}
-          </h1>
-          <div className='flex gap-10 items-center '>
-            <GrFormPrevious
-              className='w-5 h-5 cursor-pointer hover:scale-105 transition-all'
-              onClick={() => {
-                setToday(today.month(today.month() - 1))
-              }}
-            />
-            <h1
-              className=' cursor-pointer hover:scale-105 transition-all'
-              onClick={() => {
-                setToday(currentDate)
-              }}>
-              Mes
-            </h1>
-            <GrFormNext
-              className='w-5 h-5 cursor-pointer hover:scale-105 transition-all'
-              onClick={() => {
-                setToday(today.month(today.month() + 1))
-              }}
-            />
-          </div>
-        </div>
-        <div className='grid grid-cols-7 '>
-          {days.map((day, index) => {
-            return (
-              <h1
-                key={index}
-                className='text-sm text-center h-14 w-14 grid place-content-center text-gray-500 select-none'>
-                {day}
-              </h1>
-            )
-          })}
-        </div>
-
-        <div className='grid grid-cols-7'>
-          {generateDate(today.month(), today.year()).map(
-            ({ date, currentMonth, today }, index) => {
-              return (
+    <div className='pt-16'>
+      <div className='max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-6'>
+        <div className='md:grid md:grid-cols-2 md:divide-x md:divide-gray-200'>
+          <div className='md:pr-14'>
+            <div className='flex items-center'>
+              <h2 className='flex-auto font-semibold text-gray-900 capitalize'>
+                {format(firstDayCurrentMonth, 'MMMM yyyy', {
+                  locale: es
+                })}
+              </h2>
+              <button
+                type='button'
+                onClick={previousMonth}
+                className='-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500'>
+                <span className='sr-only'>
+                  Mes anterior
+                </span>
+                <ChevronLeftIcon
+                  className='w-5 h-5'
+                  aria-hidden='true'
+                />
+              </button>
+              <button
+                onClick={nextMonth}
+                type='button'
+                className='-my-1.5 -mr-1.5 ml-2 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500'>
+                <span className='sr-only'>
+                  Mes siguiente
+                </span>
+                <ChevronRightIcon
+                  className='w-5 h-5'
+                  aria-hidden='true'
+                />
+              </button>
+            </div>
+            <div className='grid grid-cols-7 mt-10 text-xs leading-6 text-center text-gray-500'>
+              <div>Do</div>
+              <div>Lu</div>
+              <div>Ma</div>
+              <div>Mi</div>
+              <div>Ju</div>
+              <div>Vi</div>
+              <div>Sa</div>
+            </div>
+            <div className='grid grid-cols-7 mt-2 text-sm'>
+              {days.map((day, dayIdx) => (
                 <div
-                  key={index}
-                  className='p-2 text-center h-14 grid place-content-center text-sm border-t'>
-                  <h1
-                    className={cn(
-                      currentMonth ? '' : 'text-gray-400',
-                      today ? 'bg-red-600 text-white' : '',
-                      selectDate.toDate().toDateString() ===
-                        date.toDate().toDateString()
-                        ? 'bg-black text-white'
-                        : '',
-                      'h-10 w-10 rounded-full grid place-content-center hover:bg-black hover:text-white transition-all cursor-pointer select-none'
+                  key={day.toString()}
+                  className={classNames(
+                    dayIdx === 0 &&
+                      colStartClasses[getDay(day)],
+                    'py-1.5'
+                  )}>
+                  <button
+                    type='button'
+                    onClick={() => setSelectedDay(day)}
+                    className={classNames(
+                      isEqual(day, selectedDay) &&
+                        'text-white',
+                      !isEqual(day, selectedDay) &&
+                        isToday(day) &&
+                        'text-red-500',
+                      !isEqual(day, selectedDay) &&
+                        !isToday(day) &&
+                        isSameMonth(
+                          day,
+                          firstDayCurrentMonth
+                        ) &&
+                        'text-gray-900',
+                      !isEqual(day, selectedDay) &&
+                        !isToday(day) &&
+                        !isSameMonth(
+                          day,
+                          firstDayCurrentMonth
+                        ) &&
+                        'text-gray-400',
+                      isEqual(day, selectedDay) &&
+                        isToday(day) &&
+                        'bg-red-500',
+                      isEqual(day, selectedDay) &&
+                        !isToday(day) &&
+                        'bg-gray-900',
+                      !isEqual(day, selectedDay) &&
+                        'hover:bg-gray-200',
+                      (isEqual(day, selectedDay) ||
+                        isToday(day)) &&
+                        'font-semibold',
+                      'mx-auto flex h-8 w-8 items-center justify-center rounded-full'
+                    )}>
+                    <time
+                      dateTime={format(day, 'yyyy-MM-dd')}>
+                      {format(day, 'd')}
+                    </time>
+                  </button>
+
+                  <div className='w-1 h-1 mx-auto mt-1'>
+                    {meetings.some((meeting) =>
+                      isSameDay(
+                        parseISO(meeting.startDatetime),
+                        day
+                      )
+                    ) && (
+                      <div className='w-1 h-1 rounded-full bg-sky-500'></div>
                     )}
-                    onClick={() => {
-                      setSelectDate(date)
-                    }}>
-                    {date.date()}
-                  </h1>
+                  </div>
                 </div>
-              )
-            }
-          )}
+              ))}
+            </div>
+          </div>
+          <section className='mt-12 md:mt-0 md:pl-14'>
+            <h2 className='font-semibold text-gray-900'>
+              Eventos{' '}
+              <time
+                dateTime={format(
+                  selectedDay,
+                  'yyyy-MM-dd'
+                )}>
+                {format(selectedDay, 'dd MMM yyy', {
+                  locale: es
+                })}
+              </time>
+            </h2>
+            <ol className='mt-4 space-y-1 text-sm leading-6 text-gray-500'>
+              {selectedDayMeetings.length > 0 ? (
+                selectedDayMeetings.map((meeting) => (
+                  <Meeting
+                    meeting={meeting}
+                    key={meeting.id}
+                  />
+                ))
+              ) : (
+                <p>No hay eventos para hoy.</p>
+              )}
+            </ol>
+          </section>
         </div>
-      </div>
-      <div className='h-96 w-96 sm:px-5'>
-        <h1 className=' font-semibold'>
-          Eventos para el{' '}
-          {selectDate.toDate().toLocaleDateString()}
-        </h1>
-        <p className='text-gray-400'>
-         Evento del día
-        </p>
       </div>
     </div>
   )
 }
+
+function Meeting({ meeting }) {
+  let startDateTime = parseISO(meeting.startDatetime)
+  let endDateTime = parseISO(meeting.endDatetime)
+
+  return (
+    <li className='flex items-center px-4 py-2 space-x-4 group rounded-xl focus-within:bg-gray-100 hover:bg-gray-100'>
+      <img
+        src={meeting.imageUrl}
+        alt=''
+        className='flex-none w-10 h-10 rounded-full'
+      />
+      <div className='flex-auto'>
+        <p className='text-gray-900'>{meeting.name}</p>
+        <p className='mt-0.5'>
+          <time dateTime={meeting.startDatetime}>
+            {format(startDateTime, 'h:mm a')}
+          </time>{' '}
+          -{' '}
+          <time dateTime={meeting.endDatetime}>
+            {format(endDateTime, 'h:mm a')}
+          </time>
+        </p>
+      </div>
+      <Menu
+        as='div'
+        className='relative opacity-0 focus-within:opacity-100 group-hover:opacity-100'>
+        <div>
+          <Menu.Button className='-m-2 flex items-center rounded-full p-1.5 text-gray-500 hover:text-gray-600'>
+            <span className='sr-only'>Abrir optiones</span>
+            <DotsVerticalIcon
+              className='w-6 h-6'
+              aria-hidden='true'
+            />
+          </Menu.Button>
+        </div>
+
+        <Transition
+          as={Fragment}
+          enter='transition ease-out duration-100'
+          enterFrom='transform opacity-0 scale-95'
+          enterTo='transform opacity-100 scale-100'
+          leave='transition ease-in duration-75'
+          leaveFrom='transform opacity-100 scale-100'
+          leaveTo='transform opacity-0 scale-95'>
+          <Menu.Items className='absolute right-0 z-10 mt-2 origin-top-right bg-white rounded-md shadow-lg w-36 ring-1 ring-black ring-opacity-5 focus:outline-none'>
+            <div className='py-1'>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href='#'
+                    className={classNames(
+                      active
+                        ? 'bg-gray-100 text-gray-900'
+                        : 'text-gray-700',
+                      'block px-4 py-2 text-sm'
+                    )}>
+                    Edit
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href='#'
+                    className={classNames(
+                      active
+                        ? 'bg-gray-100 text-gray-900'
+                        : 'text-gray-700',
+                      'block px-4 py-2 text-sm'
+                    )}>
+                    Cancel
+                  </a>
+                )}
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </li>
+  )
+}
+
+let colStartClasses = [
+  '',
+  'col-start-2',
+  'col-start-3',
+  'col-start-4',
+  'col-start-5',
+  'col-start-6',
+  'col-start-7'
+]
